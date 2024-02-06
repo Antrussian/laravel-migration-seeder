60d9c9c2663db2468d2b4ce3205f16531102d11e
Antrussian <antonino.russo.agent@gmail.com>
2024-02-07 00:46:31 +0100

    create migration and seeder, in models, controllers and migrations. OK DB

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use App\Models\Train;
 
 class TrainSeeder extends Seeder
 {
@@ -13,12 +15,18 @@ class TrainSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        for ($i=0; $i < 100; $i++) {
+        for ($i = 0; $i < 100; $i++) {
             $newTrain = new Train();
             $newTrain->company = $faker->company();
             $newTrain->departing_station = $faker->city();
             $newTrain->arriving_station = $faker->city();
-            $newTrain->arriving_station = $faker->city();
+            $newTrain->departing_time = $faker->time();
+            $newTrain->arriving_time = $faker->time();
+            $newTrain->train_code = $faker->regexify('[A-Z]{10}[0-9]{8}');
+            $newTrain->wagons_no = $faker->numberBetween(1, 15);
+            $newTrain->on_time = $faker->boolean(30);
+            $newTrain->cancelled = $faker->boolean(15);
+            $newTrain->save();
         }
     }
 }
